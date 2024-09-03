d66245ac74ef4487a50294129fa8655f0f723d2d
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2024-09-03 10:16:29 +0200

    iygihiu

---
+++ bashman/Program.cs
@@ -0,0 +1,14 @@
+﻿Skill attack = new(){
+    Name = "attack enemy",
+    ManaCost = 10
+};
+Skill heal = new()
+{
+    Name = "Heal self",
+    ManaCost = 20
+};
+
+Skill active = attack;
+
+Console.WriteLine(active.Name);
+Console.ReadLine();
